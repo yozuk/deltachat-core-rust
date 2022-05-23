2ff8e74b604610d6892529d9fc25593e84a6c1ed
Hocuri <hocuri@gmx.de>
2022-05-23 14:47:32 +0200

    link2xt's review

--- src/webxdc.rs
+++ src/webxdc.rs
@@ -267,7 +267,7 @@ impl Context {
         }
 
         let status_update_serial = self
-            .internal_write_status_update(&instance.id, status_update_item)
+            .write_status_update_inner(&instance.id, status_update_item)
             .await?;
 
         self.emit_event(EventType::WebxdcStatusUpdate {
@@ -278,7 +278,7 @@ impl Context {
         Ok(status_update_serial)
     }
 
-    pub(crate) async fn internal_write_status_update(
+    pub(crate) async fn write_status_update_inner(
         &self,
         instance_id: &MsgId,
         status_update_item: StatusUpdateItem,
